41cb2c4ba82c96c33fa517be934e5c5e7e1e8e44
Tony Schick <schick.anthony@gmail.com>
2016-01-21 21:47:44 -0800

    initial commit

---
+++ grazing/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+
+# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=30)
+    abbr = models.CharField(max_length=2)
+
+class FieldOffice(models.Model):
+    state = models.ForeignKey("State")
+    office_code = models.CharField(max_length=55)
+    office_name = models.CharField(max_length=255)
+
+class Operator(models.Model):
+
+    field_office = models.ForeignKey("FieldOffice")
+    auth_no = models.CharField(max_length=255)
+    operator_display_name = models.CharField(max_length=255)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    st2 = models.CharField(max_length=255)
+    zipcode15 = models.CharField(max_length=255)
+    zipcode69 = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    release_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "State: {}, Allotment name {},".format(self.admin_state,  self.allotment_name)
+
+class Allotment(models.Model):
+
+    field_office = models.ForeignKey("FieldOffice")
+    allotment_number = models.CharField(max_length=11)
+    allotment_id = models.CharField(max_length=255)
+    allotment_name = models.CharField(max_length=255)
+    available_for_grazing = models.CharField(max_length=1)
+    grazing_decision = models.CharField(max_length=255)
+    public_acres = models.IntegerField()
+    amp_text = models.CharField(max_length=255)
+    amp_implement_date = models.DateTimeField()
+    management_stat_text = models.CharField(max_length=255)
+    auth_no	= models.ManyToMany("Operator")
+    permitted_aums = models.IntegerField()
+    suspended_aums = models.IntegerField()
+    susp_use_temp = models.CharField(max_length=10)
+    new_allotment_id = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "Allotment name: {}, State: {}".format(self.allotment_name,  self.field_office.state)
+
+class Permit(models.Model):
+
+    field_office = models.ForeignKey("FieldOffice")
+    auth_no = models.ForeignKey("Operator")
+    pl_effect_dt = models.DateTimeField()
+    pl_exp_dt = models.DateTimeField()
+    permit_status = models.CharField(max_length=50)
+    allotment = models.ForeignKey("Allotment")
+    livestock_number = models.IntegerField()
+    livestock_kind = models.CharField(max_length=25)
+    pd_beg_dt = models.DateTimeField()
+    pd_end_dt = models.DateTimeField()
+    type_use = models.CharField(max_length=50)
+    pl_percent = models.IntegerField()
+    aums = models.IntegerField()
+
+    def __str__(self):
+        return self.auth_no.operator_display_name
+
+class Health(models.Model):
+
+    field_office = models.ForeignKey("FieldOffice")
+    allotment = models.ForeignKey("Allotment")
+    auth_no = models.ForeignKey("Operator")
+    land_health_eval_date = models.DateTimeField(null=True,blank=True) # Date of Most Recent Land Health Evaluation Report (mm/dd/yyyy)1
+    causal_factors_date = models.DateTimeField(null=True,blank=True) #Date of most recent Determination of Causal Factor(s) (mm/dd/yy
+    standards_not_met = models.ForeignKey("Standards") # Land Health Standard(s) Not Achieved in the Allotment and Signi	text	1
+    nepa_type = models.ForeignKey("NEPAType") #Type of NEPA Analysis for Grazing Authorization (EA, EIS, CX, D
+    nepa_date = models.DateTimeField(null=True,blank=True) #Date NEPA Analysis Completed (mm/dd/yyyy)5
+    nepa_identifier = models.CharField(max_length=55) #NEPA Identifier6
+    permit_status = models.CharField(max_length=55) #Permit or Lease Status
+
+    def __str__(self):
+        return self.allotment.allotment_name
---
+++ rangeland/settings.py
@@ -0,0 +1,85 @@
+"""
+Django settings for rangeland project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!cd=j57i)2o0%!x(kg29q!7(@gdi+6ov7q!z_a6&-r*^2f*nyb'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'grazing'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'rangeland.urls'
+
+WSGI_APPLICATION = 'rangeland.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
