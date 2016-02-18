84a6145e92da160a8efc031893ba37d0ebd3f249
Tony Schick <schick.anthony@gmail.com>
2016-02-18 01:25:33 -0800

    template fixes to allotment_detail

--- grazing/views.py
+++ grazing/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Sum
 from django.db.models import Count
 
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import *
+
 
 from grazing.models import *
 
@@ -17,27 +20,9 @@ def index(request):
 	exempted_permits = State.objects.filter(fieldoffice__permit__permit_status='FLPMA 402(C)(2)/APPROP ACT').aggregate(total=Count('fieldoffice__permit'))
 	exempted_permit_pct = (exempted_permits['total'] / all_permits['total']) * 100
 
-
-	# for s in states:
-
-
-
-
-	# for each state:
-
-
-	#total ranchers
-
-	#total livestock / aums
-
-
-	# permits renewed without review
-
-	#
-
 	return render(request, 'base.html', {'states': states, 'acreage': acreage, 'exempted_permit_pct': exempted_permit_pct})
 
-# We're likely not using this anymore
+# state by state view, used to generate the descriptions used on the index page of the published graphic
 def state(request, state_slug):
 
 	state = State.objects.get(abbr=state_slug)
@@ -63,26 +48,42 @@ def state(request, state_slug):
 
 	return render(request, 'state_detail.html', {'state': state, 'acreage': acreage, 'livestock': livestock, 'ranchers': ranchers, 'all_permits': all_permits, 'exempted_permits': exempted_permits, 'exempted_permit_pct': exempted_permit_pct, 'lhs_acres_yes': lhs_acres_yes, 'lhs_acres_unk': lhs_acres_unk})
 
+
 def allotment(request, allotment_unique):
+
+	boundary = Health.objects.get(allotment_unique=allotment_unique)
+
 	allot = Allotment.objects.get(allotment_unique=allotment_unique)
+	total_acres = Allotment.objects.filter(allotment_unique=allotment_unique).aggregate(total=Sum('public_acres'))
+
+	ranchers = Allotment.objects.filter(allotment_unique=allotment_unique).aggregate(total=Count('auth_no__operator__id'))
+
+	permits = Permit.objects.filter(allotment__allotment_unique=allotment_unique)
+	all_permits = Permit.objects.filter(allotment__allotment_unique=allotment_unique).aggregate(total=Count('id'))
+
+	exempted_permits = Permit.objects.filter(allotment__allotment_unique=allotment_unique, permit_status='FLPMA 402(C)(2)/APPROP ACT').aggregate(total=Count('id'))
+	exempted_permit_pct = (exempted_permits['total'] / all_permits['total']) * 100 # hack to get this into readable pct format
+
+
+
+	operators = Operator.objects.filter(auth_no__permit__allotment__allotment_unique=allotment_unique).distinct()
+
 	auths = allot.auth_no.all()
-
-	operators = []
-
-	for a in auths:
-		o = a.operator_set.all()
-
-
-		operators.extend(o)
-
 
 	health = allot.health_set.all()
 
+	return render(request, 'allotment_detail.html',
+		{
+		'allot': allot,
+		'auths': auths,
+		'health': health,
+		'operators': operators,
+		'boundary': boundary,
+		'total_acres': total_acres,
+		'ranchers': ranchers,
+		'permits': permits,
+		'all_permits': all_permits,
+		'exempted_permits': exempted_permits,
+		'exempted_permit_pct': exempted_permit_pct,
 
-	return render(request, 'allotment_detail.html', {'allot': allot, 'auths': auths, 'health': health, 'operators': operators})
-
-
-
-# Create your views here.
-# Create your views here.
-#from django.http import HttpResponseRedirect
+			})
